6a6191132bcd78d1108945eb9e35d0c23aeeb301
tvhfrks12678 <tvhfrks12678@gmail.com>
2021-01-04 15:15:01 +0900

    finish-chapter-4

---
+++ spec/factories/projects.rb
@@ -0,0 +1,24 @@
+FactoryBot.define do
+  factory :project do
+    sequence(:name) { |n| "Project #{n}" }    
+    description "Sample project for testing purposes"
+    due_on 1.week.from_now
+    association :owner
+
+    trait :with_notes do
+      after(:create) { |project| create_list(:note, 5, project: project) }
+    end
+
+    trait :project_due_yesterday do
+      due_on 1.day.ago
+    end
+
+    trait :project_due_today do
+      due_on Date.current.in_time_zone
+    end
+
+    trait :project_due_tomorrow do
+      due_on 1.day.from_now
+    end
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
+  it "has a valid factory" do
+    expect(FactoryBot.build(:user)).to be_valid
+  end
+
   it "is valid whith a first name, last name, email, and password" do
 
     user = User.new(
@@ -13,49 +18,38 @@ RSpec.describe User, type: :model do
   end
 
   it "is invalid without a first name" do
-
-    user = User.new(first_name: nil)
-
+    user = FactoryBot.build(:user, first_name: nil)
     user.valid?
-
     expect(user.errors[:first_name]).to include("can't be blank")
   end
 
   it "is invalid without a last name" do
-    user = User.new(last_name: nil)
-
+    user = FactoryBot.build(:user, last_name: nil)
     user.valid?
-
     expect(user.errors[:last_name]).to include("can't be blank")
   end
 
   it "is invalid without an email address" do
-    User.create(
-      first_name: "Joe",
-      last_name: "Tester",
-      email: "tester@example.com",
-      password: "dottle-nouveau-pavilion-tights-furze",
-    )
+    user = FactoryBot.build(:user, email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
 
-    user = User.new(
-      first_name: "Jane",
-      last_name: "Tester",
-      email: "tester@example.com",
-      password: "dottle-nouveau-pavilion-tights-furze",
-    )
+  it "is invalid with a duplicate email address" do
+    FactoryBot.create(:user, email: "aaron@example.com")
+    user = FactoryBot.build(:user, email: "aaron@example.com")
     user.valid?
     expect(user.errors[:email]).to include("has already been taken")
   end
 
-  it "is invalid with a duplicate email address"
-
-
   it "returns a user's full name as a string" do
-    user = User.new(
-      first_name: "John",
-      last_name: "Doe",
-      email: "johndoe@example.com",
-    )
+    user = FactoryBot.build(:user, first_name: "John", last_name: "Doe")
     expect(user.name).to eq "John Doe"
+  end
+
+  it "does something with multiple users" do
+    user1 = FactoryBot.create(:user)
+    user2 = FactoryBot.create(:user)
+    expect(true).to  be_truthy
   end
 end
