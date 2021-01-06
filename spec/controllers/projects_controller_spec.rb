70d396b80f9094854efb91f241c08f62bce3f6d6
tvhfrks12678 <tvhfrks12678@gmail.com>
2021-01-06 10:56:41 +0900

    finish-chapter-5-exercise

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -63,6 +63,68 @@ RSpec.describe ProjectsController, type: :controller do
         end
     end
 
+    describe "#new" do
+        context "as an authenticated user" do
+            before do
+                @user = FactoryBot.create(:user)
+            end
+            
+            it "responds successfully" do
+                sign_in @user
+                get :new
+                expect(response).to be_success  
+            end
+    
+            it "return a 200 response" do
+                sign_in @user
+                get :new
+                expect(response).to have_http_status "200"  
+            end
+        end
+
+        context "as a guest" do
+            it "returns a 302 response" do
+                get :new
+                expect(response).to  have_http_status "302"
+            end
+
+            it "redirects to the sign-in page" do
+                get :new
+                expect(response).to  redirect_to "/users/sign_in"
+            end
+        end
+    end
+
+    describe "#edit" do
+        context "as an authorized user" do
+            before do 
+                @user = FactoryBot.create(:user)
+                @project = FactoryBot.create(:project, owner: @user)
+                
+            end
+
+            it "responds successfully" do
+                sign_in @user
+                get :edit, params: { id: @project.id }
+                expect(response).to  be_success
+            end
+        end
+
+        context "as an unauthorized usr" do
+            before do
+                @user = FactoryBot.create(:user)
+                other_user = FactoryBot.create(:user)
+                @project = FactoryBot.create(:project, owner: other_user)
+            end
+
+            it "redirects to the dashboard" do
+                sign_in @user
+                get :edit, params: { id: @project.id }
+                expect(response).to redirect_to root_path
+            end
+        end
+    end
+
     describe "#create" do
         context "as an authenticated user" do
             before do
